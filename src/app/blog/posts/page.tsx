84fb978f0d1577ee3a3a12c192e945041ca47ab1
drv0228 <110354201+drv0228@users.noreply.github.com>
2024-09-02 07:49:13 -0400

    Setting up a PostgreSQL database and successfully connected to database

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,15 +1,15 @@
 import { posts } from "@/app/lib/placeholder-data";
 import Post from "@/app/ui/components/posts/Post";
+import { connectToDB } from '@/app/lib/data';
 
-export default function Page() {
+export default async function Page() {
+   const client = await connectToDB();
+
 return (
  <>
+  {client && <p className='text-green-500'>Connected to database</p>}
  <h1>Lastest Posts</h1>
- 
-    {posts.map((post) => <Post key={post.id} {...post} />)}
-
- 
- </>
+   {posts.map((post) => <Post key={post.id} {...post} />)}</>
 );
 }
 
